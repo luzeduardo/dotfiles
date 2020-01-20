72e366db27f77e58b003232c49f887b6a03246ab
Eduardo <luz.eduardo@gmail.com>
2020-01-20 15:59:56 -0300

    zsh conf as default

--- fedora/dotfiles.sh
+++ fedora/dotfiles.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-sudo dnf -y update
-sudo dnf -y install git vim gvim terminator udisks wget
-sudo dnf -y install gnome-tweak-tool dnf-plugins-core openssh-server
-
-sudo dnf -y install wget make autoconf texinfo
-sudo dnf -y install npm
-
 title () {
    echo "********************************************************"
    echo "********************************************************"
@@ -14,35 +6,100 @@ title () {
    echo "********************************************************"
    echo "********************************************************"
 }
+ssh-keygen -t rsa -N '' -C "luz.eduardo@gmail.com"
+ssh-add ~/.ssh/id_rsa
 
-# Shell
+sudo dnf -y update
+sudo dnf -y install git vim gvim terminator udisks wget
+sudo dnf -y install gnome-tweak-tool dnf-plugins-core openssh-server
+sudo dnf -y install wget make autoconf texinfo
+sudo dnf -y install npm
+sudo dnf -y install zlib-devel bzip2 bzip2-devel readline-devel openssl-devel xz xz-devel bison bison-devel glibc-devel binutils gcc autoconf automake gcc-c++ libffi-devel libtool sqlite-devel libyaml-devel xclip
+
+# ranger (and utilities for previewing files)
+title "utils"
+sudo dnf -y install ranger caca-utils highlight atool w3m poppler-utils mediainfo alsa-lib-devel
+title "utils-END"
+
 title "shell"
 sudo dnf install htop curl httpie bash bash-completion tmux vim
 title "shell-END"
 
-# Laptop
 title "laptop"
-sudo dnf -y install acpi powertop tlp xclip xdotool recode
+sudo dnf -y install acpi powertop tlp xdotool recode
 title "laptop-END"
 
-# gpg
-title "gpg"
-sudo yum install gnupg
-title "gpg-END"
+#translate shell
+title "translateshell"
+if [[ "$(which trans)" == '' ]]; then
+  sudo dnf -y install translate-shell
+fi
+title "translateshell-END"
 
+#git flow
+title "gitflow"
+if [[ "$(which gitflow)" == '' ]]; then
+  sudo dnf install -y gitflow
+fi
+title "gitflow-END"
+
+title "gpg"
+if [[ "$(which gpg)" == '' ]]; then
+  sudo yum install gnupg
+fi
+title "gpg-END"
 
 #package manager
 title "snapd"
-sudo dnf install -y snapd
-sudo ln -s /var/lib/snapd/snap /snap
+if [[ "$(which snap)" == '' ]]; then
+  sudo dnf install -y snapd
+  sudo ln -s /var/lib/snapd/snap /snap
+fi
 title "snapd-END"
+
+#silver searcher
+title "silversearcher"
+sudo dnf install -y the_silver_searcher
+title "silversearcher-END"
+
+# Java
+title "java"
+if [[ "$(which java)" == '' ]]; then
+  sudo dnf -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
+fi
+title "java-END"
+
+# misc cmd line tools
+title "swish-e catalog"
+sudo dnf -y install pass swish-e
+title "swish-e catalog-END"
+
+title "zsh"
+sudo dnf -y install zsh
+grep $USER /etc/passwd
+chsh -s $(which zsh)
+grep $USER /etc/passwd
+title "zsh-END"
 
 git config --global user.name "Eduardo Luz"
 git config --global user.email "luz.eduardo@gmail.com"
 git config --global push.default simple
 
-cp ~/dotfiles/fedora/know_hosts.gpg ~/.ssh/know_hosts.gpg
+cd ~
+#dotfiles config
+mkdir -p ~/code && git clone https://github.com/luzeduardo/dotfiles ~/code/dotfiles
+cd ~/code/dotfiles 
+git remote rm origin && git remote add origin git@github.com:luzeduardo/dotfiles
+cd ~
+
+mkdir -p /usr/local/bin
+
+title "ssh"
+cp ~/dotfiles/fedora/known_hosts.gpg ~/.ssh/known_hosts.gpg
 cp ~/dotfiles/fedora/id_rsa.pub.gpg ~/.ssh/id_rsa.pub.gpg
+title "ssh-END"
+
+cd ~
 
 #ngrok
 title "NGROK"
@@ -61,50 +118,17 @@ fi
 #ngrok
 title "NGROK-END"
 
-cd ~
-#dotfiles config
-git clone https://github.com/luzeduardo/dotfiles
-cd ~/dotfiles
-git remote rm origin && git remote add origin git@github.com:luzeduardo/dotfiles
-
-mkdir -p /usr/local/bin
-mkdir -p ~/code
-
-rm ~/.bashrc
-ln -s ~/code/dotfiles/fedora/bashrc ~/.bashrc
-ln -s ~/code/dotfiles/fedora/zshrc ~/.zshrc
-
-cd ~
-
-#translate shell
-title "translateshell"
-sudo dnf -y install translate-shell
-title "translateshell-END"
-
-#zsh
-title "zsh"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-chsh -s /bin/zsh
-title "zsh-END"
-
 #vscode
 title "vscode"
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
-sudo dnf install -y code
-cat ~/dotfiles/fedora/vscode/extensions.list | xargs -L 1 code --install-extension
-cp ~/dotfiles/fedora/vscode/settings.user ~/.vscode/
+if [[ "$(which code)" == '' ]]; then
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf check-update
+  sudo dnf install -y code
+  cat ~/dotfiles/fedora/vscode/extensions.list | xargs -L 1 code --install-extension
+  cp ~/dotfiles/fedora/vscode/settings.user ~/.vscode/
+fi
 title "vscode-END"
-
-#git flow
-title "gitflow"
-sudo dnf install -y gitflow
-title "gitflow-END"
 
 #vim
 title "vim"
@@ -114,30 +138,17 @@ make install
 cd ~
 title "vim-END"
 
-#silver searcher
-title "silversearcher"
-dnf install -y zlib-devel bzip2 bzip2-devel readline-devel openssl-devel xz xz-devel bison bison-devel glibc-devel binutils gcc autoconf automake gcc-c++ libffi-devel libtool sqlite-devel libyaml-devel xclip the_silver_searcher
-title "silversearcher-END"
-
 title "chrome and other"
-sudo dnf -y install \
+if [[ "$(which chrome)" == '' ]]; then
+  sudo dnf -y install \
      google-chrome-stable\
      gnome-tweak-tool\
      gnome-shell-extension-user-theme\
      skypeforlinux\
-     telegram-desktop\
-     transmission
+     telegram-desktop
+fi
+
 title "chrome and other-END"
-
-# Java
-title "java"
-sudo dnf -y install java-1.8.0-oracle java-1.8.0-oracle-devel java-1.8.0-openjdk java-1.8.0-openjdk-devel
-title "java-END"
-
-# misc cmd line tools
-title "swish-e catalog"
-sudo dnf -y install pass swish-e
-title "swish-e catalog-END"
 
 # dmenu with ^M working as Return
 git clone https://github.com/goldfeld/dmenu ~/dmenu
@@ -147,19 +158,25 @@ cd ~/dmenu && make && sudo make install && cd ~
 cd && git clone https://github.com/google/vroom ~/qfork/vroom
 
 # rust
-curl https://sh.rustup.rs -sSf | sh
+if [[ "$(which rustup)" == '' ]]; then
+  curl https://sh.rustup.rs -sSf | sh
+fi
 
 #js
 title "js"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash\n
+if [[ "$(which nvm)" == '' ]]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash\n
+  nvm list
+  export NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm\n[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm list
-export NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm\n[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm list
+fi
 title "js-END"
 
 #node
 title "node"
-sudo snap install node --classic
+if [[ "$(which node)" == '' ]]; then
+  sudo snap install node --classic
+fi
 title "node-END"
 
 #python
@@ -167,60 +184,61 @@ title "python"
 python3 setup.py build && sudo python3 setup.py install
 title "python-END"
 
-# ruby
-title "ruby"
-sudo dnf -y install ruby rubygems
-sudy gem install git-bump
-title "ruby-END"
-
-# ranger (and utilities for previewing files)
-title "utils"
-sudo dnf -y install ranger caca-utils highlight atool w3m poppler-utils mediainfo
-title "utils-END"
-
 title "nodemon httpserver"
-sudo npm install -g http-server nodemon
+if [[ "$(which nodemon)" == '' ]]; then
+  sudo npm install -g http-server nodemon
+fi
 title "nodemon httpserver END"
-
-sude dnf -y install alsa-lib-devel
 
 # virtualbox
 title "virtualbox"
-sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O /etc/dnf.repos.d/virtualbox.repo
-sudo dnf -y update
-sudo dnf -y install binutils qt gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
-sudo dnf -y install VirtualBox-4.2
+if [[ "$(which virtualbox)" == '' ]]; then
+  sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
+  sudo dnf -y update
+  sudo dnf -y install @development-tools
+  sudo dnf install kernel-devel kernel-headers dkms qt5-qtx11extras  elfutils-libelf-devel zlib-devel
+  sudo dnf -y install VirtualBox-6.0
+  usermod -a -G vboxusers $USER
+fi
 title "virtualbox END"
 
 # sudo dnf -y groupinstall "GNOME Desktop"
 
 # exercism config
 title "exercism"
-wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz
-tar -xf exercism-linux-64bit.tgz
-mkdir -p ~/bin
-mv exercism ~/bin
- 
-exercism configure --token=a8e70d9c-94f4-4ac0-9245-4af5c949e187
+if [[ "$(which exercism)" == '' ]]; then
+  wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-3.0.13-linux-x86_64.tar.gz
+  tar -xf exercism-linux-64bit.tgz
+  mkdir -p ~/bin
+  mv exercism ~/bin
+  
+  exercism configure --token=a8e70d9c-94f4-4ac0-9245-4af5c949e187
+fi
 title "exercism END"
 
  #yarn
  title "yarn"
- curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
- sudo yum install yarn
+ if [[ "$(which yarn)" == '' ]]; then
+  curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+  sudo yum install yarn
+ fi
  title "yarn-END"
   
  #brave browser
  title "brave"
- sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
- sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
- sudo dnf install brave-browser
+ if [[ "$(which brave)" == '' ]]; then
+  sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+  sudo dnf install brave-browser
+ fi
  title "brave-END"
    
  #tile screen manager 
  #sudo dnf install i3 i3status dmenu i3lock xbacklight feh conky
- title "tile" 
- git clone https://github.com/gTile/gTile.git ~/.local/share/gnome-shell/extensions/gTile@vibou
+ title "tile"
+ if [ ! -d ~/.local/share/gnome-shell/extensions/gTile@vibou ] 
+  git clone https://github.com/gTile/gTile.git ~/.local/share/gnome-shell/extensions/gTile@vibou
+ fi
  title "tile-END"
 
 # pdf to kindle converter
@@ -276,10 +294,39 @@ fi
 #vlc
 title "vlc-END"
 
-title "postman-END"
-sudo snap install postman
+title "postman"
+if [[ "$(which postman)" == '' ]]; then
+  sudo snap install postman
+fi
 title "postman-END"
 
+title "slack"
+if [[ "$(which slack)" == '' ]]; then
+  sudo snap install slack --classic
+fi
 title "slack-END"
-sudo snap install slack --classic
-title "slack-END"
+
+
+#pretzo
+title "pretzo"
+if [[ "$(which pretzo)" == '' ]]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+  chsh -s /bin/zsh
+fi
+title "pretzo-END"
+
+rm ~/.bashrc
+ln -s ~/dotfiles/fedora/bashrc ~/.bashrc
+ln -s ~/dotfiles/fedora/zshrc ~/.zshrc
+ln -s ~/dotfiles/fedora/zshenv ~/.zshenv
+ln -s ~/dotfiles/fedora/zpretzorc ~/.zpretzorc
+
+
+#flutter
+#android studio
+#stremio
+#deluge
