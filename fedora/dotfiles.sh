abb82959fbb9e49d9fd0e7d601aacf9c5a4219fa
Eduardo <luz.eduardo@gmail.com>
2020-02-05 23:27:18 -0300

    add brightness alias

--- fedora/dotfiles.sh
+++ fedora/dotfiles.sh
@@ -177,7 +177,7 @@ fi
 #js
 title "js"
 if [[ "$(which nvm)" == '' ]]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
   nvm list
   export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm\n[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm list
